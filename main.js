b7034f973ce8f25f82eb47902700c9c7a7d7d0e0
Haley Morgan <haleymorgan3264@gmail.com>
2019-06-24 22:01:58 -0400

    add 3 variables to story story text and arrays to generate random words

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Create 3 variables for name text input, story generation button, and the story p tag
+// Create 3 variables for references to name text input, story generation button, and the story p tag
 
 let customName = document.getElementById('customname');
 let randomize = document.querySelector('.randomize');
@@ -9,4 +9,13 @@ let story = document.querySelector('.story');
 function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
+
+
+// Create variables that will act as input to our story generator
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
 
